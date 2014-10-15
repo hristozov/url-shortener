002167e896a363c6e7e0f908a2265d8977cd70a3
Georgi Hristozov <georgi@forkbomb.nl>
2014-10-15 14:17:49 +0300

    UI for shortening.

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -1,7 +1,34 @@
 class ShortenedUrlsController < ApplicationController
-  def index
+  before_action :set_shortened_url, only: [:show]
+
+  def show
+  end
+
+  def new
+    @shortened_url = ShortenedUrl.new();
+  end
+
+  def create
+    @shortened_url = ShortenedUrl.new(shortened_url_params)
+    @shortened_url.user_id = current_user.id
+
     respond_to do |format|
-      format.html { render :index }
+      if @shortened_url.save
+        format.html { redirect_to action: :show, id: @shortened_url.id, notice: 'Yeah!' }
+      else
+        format.html { render :new }
+      end
     end
+
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_shortened_url
+    @shortened_url = ShortenedUrl.find(params[:id])
+  end
+
+  def shortened_url_params
+    params.require(:shortened_url).permit(:original)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-  root 'shortened_urls#index'
+  root 'shortened_urls#new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
